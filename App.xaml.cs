e91b0ccb6a6e33cea237a42b30df863bba6c510d
Francesco Priori <116790887+FrancescoPriori@users.noreply.github.com>
2023-02-24 09:37:49 +0100

    Ultima Versione Threads

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace priori.francesco._4h.threads
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
